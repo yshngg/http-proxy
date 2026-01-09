c6c86cc5a990a7906e8687297b43069981aca278
Yusheng Guo <yshngg@outlook.com>
2026-01-09 20:36:26 +0800

    feat: support https

--- main.go
+++ main.go
@@ -2,31 +2,86 @@ package main
 
 import (
 	"bufio"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"flag"
 	"fmt"
 	"io"
-	"log"
-	"log/slog"
 	"net"
 	"net/http"
+	"os"
 	"sync"
+
+	"k8s.io/klog/v2"
 )
 
 func main() {
-	l, err := net.Listen("tcp", ":1080")
-	if err != nil {
-		log.Fatal(err)
+	addr, cert, key, ca := ":1080", "", "", ""
+	flag.StringVar(&addr, "addr", addr, "Address the proxy server will listen on.")
+	flag.StringVar(&cert, "cert", cert, "(TLS) Use the specified certificate file when run start proxy server with HTTPS")
+	flag.StringVar(&key, "key", key, "(TLS) Use the specified key file when run start proxy server with HTTPS")
+	flag.StringVar(&ca, "ca", ca, "")
+	flag.Parse()
+
+	if err := run(addr, cert, key, ca); err != nil {
+		klog.Fatal(err)
 	}
+}
+
+func run(addr, certFile, keyFile, caFile string) error {
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("listen on %s, err: %w", addr, err)
+	}
+
+	if len(certFile) != 0 && len(keyFile) != 0 {
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			return fmt.Errorf("load x509 key pair, err: %w", err)
+		}
+
+		cfg := &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		}
+
+		if len(caFile) != 0 {
+			caPem, err := os.ReadFile(caFile)
+			if err != nil {
+				return fmt.Errorf("read CA file, err: %w", err)
+			}
+			block, _ := pem.Decode(caPem)
+			caCert, err := x509.ParseCertificate(block.Bytes)
+			if err != nil {
+				return fmt.Errorf("parse x509 CA cert, err: %w", err)
+			}
+			pool, err := x509.SystemCertPool()
+			if err != nil {
+				return fmt.Errorf("get system cert poll, err: %w", err)
+			}
+			pool.AddCert(caCert)
+			cfg.RootCAs = pool
+
+			klog.Infof("use CA cert file: %s", caFile)
+		}
+
+		l = tls.NewListener(l, cfg)
+		klog.Infof("use cert file: %s and key file: %s", certFile, keyFile)
+	}
+
+	defer l.Close()
+	klog.Infoln("listen on:", l.Addr())
 
 	for {
 		localConn, err := l.Accept()
 		if err != nil {
-			slog.Warn("accept connection", slog.Attr{Key: "err", Value: slog.AnyValue(err)})
+			klog.Warningf("accept connection, err: %v", err)
 			continue
 		}
 		go func() {
 			err := handleConn(localConn)
 			if err != nil {
-				slog.Warn("handle connection", slog.Attr{Key: "err", Value: slog.AnyValue(err)})
+				klog.Warningf("handle connection, err: %v", err)
 			}
 		}()
 	}
@@ -39,9 +94,9 @@ func handleConn(conn net.Conn) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println("Request:", req.Method, req.Proto, req.Host)
+	klog.Infoln("Request:", req.Method, req.Proto, req.Host)
 	for k, v := range req.Header {
-		fmt.Println("<", k, v)
+		klog.Infof("< %s: %v", k, v)
 	}
 
 	host := req.Host
@@ -87,9 +142,9 @@ func handleConn(conn net.Conn) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println("Response:", resp.StatusCode, resp.Proto)
+	klog.Infoln("Response:", resp.StatusCode, resp.Proto)
 	for k, v := range resp.Header {
-		fmt.Println(">", k, v)
+		klog.Infof("> %s: %v", k, v)
 	}
 	resp.Write(conn)
 	return nil
